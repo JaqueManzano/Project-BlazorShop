a81ff64a26f2c5ee299e12a4b2b59cd8cf6e3330
Jaqueline Aparecida Manzano dos Santos <143463393+JaqueManzano@users.noreply.github.com>
2023-09-19 10:27:24 -0300

    Add files via upload
    Commit Data, Pages e Models

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazingShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingShop.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().ToTable("Categories");
+			modelBuilder.Entity<Product>().ToTable("Products");
+		}
+	}
+}

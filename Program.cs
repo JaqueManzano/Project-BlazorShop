ca4ec954271649f6d7ebf7dc219c3c9ef3faf031
Jaqueline Aparecida Manzano dos Santos <143463393+JaqueManzano@users.noreply.github.com>
2023-09-19 10:25:28 -0300

    Add files via upload
    Commit inicial com arquivos configuração

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using BlazingShop.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(connection));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
